fdf27b1150ce625064bb10e935284a912d9f166c
Magellan <chenming@firstgrid.cn>
2021-03-11 11:08:38 +0800

    单个组件

---
+++ src/TestHaHa.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default class TestHaHa extends React.Component{
+  render(){
+    return <div>TestHaHa</div>
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TestHaHa from './TestHaHa.js';
+module.exports={
+    TestHaHa
+}
